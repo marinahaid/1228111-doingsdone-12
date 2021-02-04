c809ee1b7feb621f6cfd06236c31128c2e375aa7
marinahaid <marinaniksol@gmail.com>
2021-02-04 15:09:56 +0100

    module3-task1

--- index.php
+++ index.php
@@ -1,90 +1,12 @@
 <?php
-require_once('utils.php');
-$show_complete_tasks = rand(0, 1);
-?>
-
-<?php
-$categories = [
-    'inbox' => 'Входящие',
-    'study' => 'Учеба',
-    'work' => 'Работа',
-    'homework' => 'Домашние дела',
-    'car' => 'Авто'
-];
-
-$tasks = [
-    [
-        'title' => 'Собеседование в IT компании',
-        'date' => '01.12.2019',
-        'category' => $categories['work'],
-        'is_complete' => false
-    ],
-
-    [
-        'title' => 'Выполнить тестовое задание',
-        'date' => '25.12.2019',
-        'category' => $categories['work'],
-        'is_complete' => false
-    ],
-
-    [
-        'title' => 'Сделать задание первого раздела',
-        'date' => '21.12.2019',
-        'category' => $categories['study'],
-        'is_complete' => true
-    ],
-
-    [
-        'title' => 'Встреча с другом',
-        'date' => '22.12.2019',
-        'category' => $categories['inbox'],
-        'is_complete' => false
-    ],
-
-    [
-        'title' => 'Купить корм для кота',
-        'date' => null,
-        'category' => $categories['homework'],
-        'is_complete' => false
-    ],
-
-    [
-        'title' => 'Заказать пиццу',
-        'date' => null,
-        'category' => $categories['homework'],
-        'is_complete' => false
-    ]
-];
-
-$config = [
-	'sitename' => 'Дела в порядке',
-	'tpl_path' => 'tpl/',
-	// сайт доступен и работает
-	'enable' => true
-];
-
-function include_template($name, array $data = []) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
+require_once('data.php');
+require_once('helpers.php');
 
 $content_main = include_template('main.php', [
     'categories' => $categories,
      'tasks' => $tasks]);
 
   $layout = include_template('layout.php', [
-    'content' => $content_main]);
+    'title' => $title, 'content' => $content_main]);
     echo $layout;
 ?>
