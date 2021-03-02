008a0098f9b732f8104c95f92b7ce3a29c9c77f4
marinahaid <marinaniksol@gmail.com>
2021-03-02 18:24:50 +0100

    database file

--- database.php
+++ database.php
@@ -1,41 +1,40 @@
 <?php
 
-//$link = mysqli_connect('localhost', 'root', 'root', 'doings');
+/**
+ * @param const SERVER_NAME string
+ * @param const DATA_BASE string
+ * @param const USER_NAME string
+ * @param const PASSWORD string
+ * @param function mysqli_connect получить ресурс соединения
+ */
+const SERVER_NAME = 'localhost';
+const DATA_BASE = 'doings';
+const USER_NAME = 'root';
+const PASSWORD = 'root';
 
-//if(mysqli_connect_errno()) {
-   // echo 'Ошибка подключения к базе данных ('.mysqli_connect_errno().') :' .mysqli_connect_error();
-   // exit();
-//}
-
-$servername = 'localhost';
-$database = 'doings';
-$username = 'root';
-$password = 'root';
-$con = mysqli_connect('localhost','root', 'root', 'doings' );
+$con = mysqli_connect(SERVER_NAME, USER_NAME, PASSWORD, DATA_BASE);
 if (!$con) {
-    die ("Connection failed:" . mysqli_connect_error());
-    }
-    echo "Connected successfully";
-    mysqli_close($con);
-
-
-function select_category() {
-    $query = "SELECT name, users_id FROM categories WHERE users_id = 1";//Отправьте SQL-запрос для получения списка проектов у текущего пользователя
-    $res = mysqli_query($con, $query);
-
-    $row = mysqli_fetch_all($res, MYSQLI_ASSOC);
-  // foreach ($rows as $row) {
-       // print ("Пользователь: " . $row['users_id'] . "; Идентификатор: . " . $row['id'] . "; Проект: . " . $row['name'] . "<br>");
-       return $row;
-    }
-
-function select_tasks() {
- $query = "SELECT put_date, status, deadline FROM tasks WHERE category_id = 1;"; //Отправьте SQL-запрос для получения списка из всех задач у текущего пользователя.
- $res = mysqli_query($con, $query);
-
- $row = mysqli_fetch_all($res, MYSQLI_ASSOC);
-// foreach ($rows as $row) {
-     return $row;
+    die("Connection failed:" . mysqli_connect_error());
 }
 
 
+
+function select_categories()
+{
+    $query = "SELECT name, id FROM categories WHERE users_id = 1"; //Отправьте SQL-запрос для получения списка проектов у текущего пользователя
+    $res = mysqli_query($con, $query);
+    $row = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    return ($row);
+}
+$row->$categories;
+
+
+
+function select_tasks($categories_id)
+{
+    $query = "SELECT put_date, status, deadline FROM tasks WHERE category_id = 1;"; //Отправьте SQL-запрос для получения списка из всех задач у текущего пользователя.
+    $res = mysqli_query($con, $query);
+    $rows = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    $rows->tasks;
+}
+select_tasks();
--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $show_complete_tasks = rand(0, 1);
 $content_main = include_template('main.php', [
   'categories' => $categories,
   'tasks' => $tasks,
-  'show_complete_tasks' => $show_complete_tasks
-]);
+  'show_complete_tasks' => $show_complete_tasks,
+  ]);
 
 $layout = include_template('layout.php', [
   'title' => "Дела в порядке", 'content' => $content_main
